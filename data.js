3b5226d93e26b571c2fb9d96fa376567b2110f06
John Carlo <marasiganjohncarlo55@gmail.com>
2025-11-25 08:18:06 +0800

    added more cars to car gallery section

--- data.js
+++ data.js
@@ -2,5 +2,33 @@ var rallyCars = [
     {
         model: "Mitsubishi Evo 2",
         img: "images/evo2.jpg"
-    }
-]   
+    },
+    {
+        model: "Mitsubishi Evo 4",
+        img: "images/evo4.jpg"
+    },
+    {
+        model: "Mitsubishi Evo 6",
+        img: "images/evo6.jpg"
+    },
+    {
+        model: "Mitsubishi Evo 8",
+        img: "images/evo8.jpg"
+    },
+    {
+        model: "Mitsubishi Evo 9",
+        img: "images/evo9.jpg"
+    },
+    {
+        model: "Subaru Impreza 555",
+        img: "images/impreza555.jpg"
+    },
+    {
+        model: "Subaru Impreza 2003",
+        img: "images/impreza2003.jpg"
+    },
+    {
+        model: "Subaru Impreza 2006",
+        img: "images/impreza2006.jpg"
+    },
+];   
